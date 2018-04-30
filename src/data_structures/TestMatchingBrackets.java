0fe84c09d73b0e719b6e0da2a80b1bce421dcc7a
NY-Giants <league@WorthyMini-5.local>
2018-04-30 16:59:20 -0700

    TestMatchingBrackets(IP)
    gg

--- src/data_structures/TestMatchingBrackets.java
+++ src/data_structures/TestMatchingBrackets.java
@@ -18,9 +18,38 @@ public class TestMatchingBrackets {
 		assertFalse(doBracketsMatch("}{"));
 	}
 
-	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS A MATCHING CLOSING BRACKET
+	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS
+	// A MATCHING CLOSING BRACKET
 	private boolean doBracketsMatch(String b) {
-		return false;
+		Stack<Character> bracketmatch = new Stack<Character>();
+		for (int i = 0; i < b.length(); i++) {
+			bracketmatch.push(b.charAt(i));
+		}
+		int openCount = 0;
+		int closeCount = 0;
+		int size = bracketmatch.size();
+		System.out.println(size);
+		for (int j = 0; j < size; j++) {
+			Character poppedcharacters = bracketmatch.pop();
+			if (poppedcharacters.equals('{')) {
+				openCount++;
+				System.out.println("open"+openCount);
+			}
+
+			else if (poppedcharacters.equals('}')) {
+				closeCount++;
+				System.out.println("close"+closeCount);
+			}
+		}
+		if (openCount == closeCount) {
+			System.out.println("true");
+			return true;
+
+		} else {
+			System.out.println("false");
+			return false;
+		}
+
 	}
 
 }
